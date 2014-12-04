70da97549c26c2b60c170442a0b40a38bf9b23df
Kisha Richardson <kisha@spunklabs.com>
2014-12-03 18:36:09 -0800

    update lesson plan

---
+++ 04_Collections_APIs/starter_code/ex_teddit_basic.rb
@@ -0,0 +1,12 @@
+#TEDDIT:  Strings - Student's File
+# Teddit is a Ruby text based news aggregator. Think Reddit in your terminal.
+# This exercise will be used throughout the ruby portion of this course. 
+# Each lesson we will incrementally build a news aggregator. 
+# We will create a more dynamic Teddit, and pull the latest news stories from Mashable, Digg and Reddit. 
+# But until then we will hard code stories.
+
+#Goals 
+#1 -  Get the story title, category from user from the command line 
+#2 -  Write a conditional that determine upvotes based on the category
+#3 -  Save every story in a collection 
+#4 -  Print each stories title, category and upvotes to the screen
