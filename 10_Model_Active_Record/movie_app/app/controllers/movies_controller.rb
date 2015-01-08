5aba65f06fab4d70b84c927eff8970c3b90e46f7
Kisha Richardson <kisha@spunklabs.com>
2015-01-07 21:26:44 -0800

    add movie_app from code_along

---
+++ 10_Model_Active_Record/movie_app/app/controllers/movies_controller.rb
@@ -0,0 +1,25 @@
+class MoviesController < ApplicationController
+  
+  def index
+    @movies = Movie.all
+  end
+
+  def show
+  end
+
+  def new
+  end
+
+  def create
+  end
+
+  def edit
+  end
+
+  def update
+  end
+
+  def destroy
+  end
+
+end
