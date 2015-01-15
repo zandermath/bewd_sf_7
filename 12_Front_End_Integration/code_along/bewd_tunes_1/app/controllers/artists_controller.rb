80093da63e74a6e90ed9acd30be6df8712702ec4
Kisha Richardson <kisha@spunklabs.com>
2015-01-14 20:53:06 -0800

    add code_along

---
+++ 12_Front_End_Integration/code_along/bewd_tunes_1/app/controllers/artists_controller.rb
@@ -0,0 +1,39 @@
+class ArtistsController < ApplicationController
+  def new
+    @artist = Artist.new
+  end
+
+  def create
+    @artist = Artist.new(artist_params)
+    @artist.save
+    redirect_to artist_path(@artist)
+  end
+
+  def index
+    @artists = Artist.all
+  end
+
+  def show
+    @artist = get_artist
+  end
+
+  def edit
+  end
+
+  def destroy
+    @artist = get_artist
+    @artist.destroy
+    redirect_to artists_path 
+  end
+
+  private
+
+  def artist_params
+    params.require(:artist).permit(:name, :profile_image)
+  end
+
+  def get_artist
+    @artist = Artist.find(params[:id])
+  end
+
+end
